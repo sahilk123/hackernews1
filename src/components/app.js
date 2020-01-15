ce8fe2b5ed51a4d18ba65a2d2a2bd094f9845162
sahil <sahilk6540@gmail.com>
2020-01-15 11:05:10 +0530

    fetching hits with on console

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React from  'react';
 import Searchbar from "./Searchbar";
 import News from "./News";
+import axios from "axios";
 
 
 
@@ -8,6 +9,16 @@ import News from "./News";
 
 
 class App extends React.Component {
+
+    componentDidMount = async () => {
+        this.fetchData()
+    }
+
+    fetchData = async () => {
+        let response = await axios.get(`https://hn.algolia.com/api/v1/search?query=sports&hitsPerPage=15&page=51`);
+
+    console.log("response=>" , response);
+    }
 
 
 
