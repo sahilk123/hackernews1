f84e1b94a211f1a86a4619bf2cc5ff6a83d5b45f
sahil <sahilk6540@gmail.com>
2020-01-20 17:13:58 +0530

    loading news

--- src/components/News.js
+++ src/components/News.js
@@ -4,36 +4,46 @@ class News extends React.Component {
     render() {
 
         if (this.props.abc.length === 0) {
-            return (
-                <div>
+            return ( <
+                div >
 
-                        <div className="ui active inverted dimmer">
-                            <div className="ui text loader">Loading</div>
-                        </div>
-                        <p></p>
-                </div>
+                <
+                div className = "ui active inverted dimmer" >
+                <
+                div className = "ui text loader" > Loading... < /div> <
+                div className = "ui text loader" > Loading... < /div> <
+                /div> <
+                p > < /p> <
+                /div>
             );
         }
-        return (
-            <div className="ui segment">
-                <div className="ui relaxed divided list">
-                    {
-                        this.props.abc.map(news => {
-                            return (
-                                <div className="item">
-                                    <div className="content">
-                                        <a href={news.url} className="header">{news.title} &nbsp; <b> &nbsp; ( {news.url} )</b></a>
-                                        <div className="description"> Created AT  {news.created_at}
-                                            <a href={news.author} className="label">    &nbsp; Author:  {news.author}</a>
-                                        </div>
+        return ( <
+            div className = "ui segment" >
+            <
+            div className = "ui relaxed divided list" > {
+                this.props.abc.map(news => {
+                    return ( <
+                        div className = "item" >
+                        <
+                        div className = "content" >
+                        <
+                        a href = { news.url }
+                        className = "header" > { news.title } & nbsp; < b > & nbsp;
+                        ({ news.url }) < /b></a >
+                        <
+                        div className = "description" > Created AT { news.created_at } <
+                        a href = { news.author }
+                        className = "label" > & nbsp; Author: { news.author } < /a> <
+                        /div>
 
-                                    </div>
-                                </div>
-                            );
-                        })
-                    }
-                </div>
-            </div>
+                        <
+                        /div> <
+                        /div>
+                    );
+                })
+            } <
+            /div> <
+            /div>
         );
     }
 }
